67986a757295f97d1a2f6e1a5015f14ce1de7676
kobbin003 <dkfeto@gmail.com>
2023-07-06 21:55:24 +0530

    Callback component

---
+++ src/layout/Callback.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import useGetAccessCode from "../hooks/useGetAccessCode";
+import { useNavigate } from "react-router-dom";
+
+const Callback = () => {
+	const navigate = useNavigate();
+
+	const code = useGetAccessCode();
+
+	useEffect(() => {
+		console.log("code-callback", code);
+		if (code) {
+			console.log("code-callback-yes", code);
+			navigate("/me");
+			localStorage.setItem("code", code);
+		}
+		// else {
+		// 	console.log("code-callback-no", code);
+		// 	navigate("/");
+		// 	localStorage.removeItem("code");
+		// }
+	}, [code]);
+	return <div>Loading...</div>;
+};
+
+export default Callback;
