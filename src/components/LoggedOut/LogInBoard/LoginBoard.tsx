10579453b796a8a8c189e358af00a20b691b62aa
kobbin003 <dkfeto@gmail.com>
2023-07-06 21:52:22 +0530

    replaced click event with Link

--- src/components/LoggedOut/LogInBoard/LoginBoard.tsx
+++ src/components/LoggedOut/LogInBoard/LoginBoard.tsx
@@ -1,18 +1,25 @@
 import React, { MouseEvent } from "react";
 import { Content } from "./style";
 import { authorize } from "../../../utils/authorize";
+import { Link } from "react-router-dom";
 
 const LoginBoard = () => {
-	const handleLogin = (e: MouseEvent<HTMLAnchorElement>) => {
-		e.preventDefault();
-		authorize();
-	};
+	const oAuthUrl = authorize();
+	// const handleLogin = (e: MouseEvent<HTMLAnchorElement>) => {
+	// 	e.preventDefault();
+	// 	authorize();
+	// };
 	return (
 		<Content>
 			<div>
 				<h1>Welcome to the Spotify Clone!</h1>
 				<h2>Login to your Spotify Account</h2>
-				<a onClick={handleLogin}>Log In</a>
+				<Link
+					to={oAuthUrl}
+					// onClick={handleLogin}
+				>
+					Log In
+				</Link>
 				<div>
 					<p>Don't have a spotify account?&nbsp;</p>
 					<a href="https://www.spotify.com/us/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2F%23_%3D_">
