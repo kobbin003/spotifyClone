823a806633d71199a057c9327320dd7444b83894
kobbin003 <dkfeto@gmail.com>
2023-07-15 21:07:57 +0530

    getFollowedArtist modified

---
+++ src/hooks/spotify-data/getFollowedArtists.ts
@@ -0,0 +1,55 @@
+import useFetchData from "../useFetchData";
+
+export type FollowedArtist = {
+	artists: {
+		href: string;
+		limit: number;
+		next: string;
+		cursors: {
+			after: string;
+			before: string;
+		};
+		total: number;
+		items: FollowedArtistItem[];
+	};
+};
+export type FollowedArtistItem = {
+	external_urls: {
+		spotify: string;
+	};
+	followers: {
+		href: string;
+		total: number;
+	};
+	genres: string[];
+	href: string;
+	id: string;
+	images: [
+		{
+			url: string;
+			height: number;
+			width: number;
+		}
+	];
+	name: string;
+	popularity: number;
+	type: "artist";
+	uri: string;
+};
+export type FollowedArtistError = { status: number; message: string };
+
+const getFollowedArtist = (): {
+	data: { items: FollowedArtistItem[] } | null;
+	error: FollowedArtistError | null;
+	isLoading: boolean;
+} => {
+	console.log("getFollowedArtist");
+	const url = `https://api.spotify.com/v1/me/following?type=artist&limit=10`;
+	const { data, error, isLoading } = useFetchData<
+		FollowedArtist,
+		FollowedArtistError
+	>(url, "GET");
+	// console.log("getFollowedArtist", { data, error, isLoading });
+	return { data: data && { items: data.artists.items }, error, isLoading };
+};
+export default getFollowedArtist;
