d8445b5b307b5eef0ee25a2168f16bda8d29c419
kobbin003 <dkfeto@gmail.com>
2023-07-22 19:58:57 +0530

    getusertoptracks

--- src/hooks/spotify-data/getUserTopItems.ts
+++ src/hooks/spotify-data/getUserTopItems.ts
@@ -1,51 +1,14 @@
 import useFetchData from "../useFetchData";
-
-export interface UserTopItemsType {
-	href: string;
-	limit: number;
-	next: string;
-	offset: number;
-	previous: string;
-	total: number;
-	items: Item[];
-}
-
-export interface Item {
-	external_urls: ExternalUrls;
-	followers: Followers;
-	genres: string[];
-	href: string;
-	id: string;
-	images: Image[];
-	name: string;
-	popularity: number;
-	type: string;
-	uri: string;
-}
-
-export interface ExternalUrls {
-	spotify: string;
-}
-
-export interface Followers {
-	href: string;
-	total: number;
-}
-
-export interface Image {
-	url: string;
-	height: number;
-	width: number;
-}
+import { UserTopTracksType } from "./userTopTracks.type";
 
 export type UserTopItemsError = { status: number; message: string };
-const getUserTopItems = (type: "tracks" | "artists") => {
-	const url = `https://api.spotify.com/v1/me/top/${type}`;
+const getUserTopTracks = () => {
+	const url = `https://api.spotify.com/v1/me/top/tracks`;
 	const { data, error, isLoading } = useFetchData<
-		UserTopItemsType,
+		UserTopTracksType,
 		UserTopItemsError
 	>(url, "GET");
 	return { data, error, isLoading };
 };
 
-export default getUserTopItems;
+export default getUserTopTracks;
