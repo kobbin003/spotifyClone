a27da064a15dc0d0480b3077ff13a24fbf8d28e4
kobbin003 <dkfeto@gmail.com>
2023-07-22 19:59:43 +0530

    Usertoptracks

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,14 +1,22 @@
 import getUserProfile from "../../hooks/spotify-data/getUserProfile";
 import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import UserTopTracks from "./UserTopTracks/UserTopTracks";
 
 const Profile = () => {
 	const { data, error, isLoading } = getUserProfile();
 	console.log({ data, error, isLoading });
 	if (isLoading) return <p>Loading...</p>;
+
+	// const {
+	// 	data: topArtists,
+	// 	error: topArtistsError,
+	// 	isLoading: topArtistsLoading,
+	// } = getUserTopItems("artists");
 	return (
 		<>
 			<h2>PROFILE</h2>
 			{data && <ProfileHeader data={data} />}
+			<UserTopTracks />
 		</>
 	);
 	// return <div>Home</div>;
---
+++ src/pages/profile/UserTopTracks/UserTopTracks.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Container, TrackItem } from "./style";
+import { msToMin } from "../../../utils/msToMin";
+import getUserTopTracks from "../../../hooks/spotify-data/getUserTopItems";
+
+const UserTopTracks = () => {
+	const { data, error, isLoading } = getUserTopTracks();
+	console.log("popular tracks", data);
+	if (data) {
+		localStorage.setItem("poptracks", JSON.stringify(data));
+	}
+	return (
+		<Container>
+			<h2>Top Tracks</h2>
+			{data &&
+				data?.items.map((track, index) => (
+					<TrackItem key={track.id}>
+						<div>
+							<span>{index + 1}</span>
+							<button>
+								<img src="/icons/playTrack.svg" />
+							</button>
+						</div>
+						<div>
+							<img src={track.album.images[0].url} />
+							<div>
+								<Link to="/n">
+									<span>{track.name}</span>
+								</Link>
+							</div>
+						</div>
+
+						<div>
+							<button>
+								<img src="/icons/heart.svg" />
+							</button>
+							<span>{msToMin(track.duration_ms)}</span>
+							<button>
+								<img src="/icons/threedots.svg" />
+							</button>
+						</div>
+					</TrackItem>
+				))}
+		</Container>
+	);
+};
+
+export default UserTopTracks;
