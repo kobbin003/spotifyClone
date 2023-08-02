197e0ebf08bd74e66dabcc1b1aebdc3f2a259bee
kobbin003 <dkfeto@gmail.com>
2023-08-02 16:12:38 +0530

    like & unlike handler added

--- src/pages/artist:id/artistContents/popularTracks/PopularTracks.tsx
+++ src/pages/artist:id/artistContents/popularTracks/PopularTracks.tsx
@@ -1,45 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import getArtistTopTracks from "../../../../hooks/spotify-data/getArtistTopTracks";
 import { Link, useParams } from "react-router-dom";
 import { msToMin } from "../../../../utils/msToMin";
 import { Container, TrackItem } from "./style";
+import putData from "../../../../hooks/spotify-data/putData/putData";
+import checkUserHasTracks from "../../../../hooks/spotify-data/checkUserHasTracks/checkUserHasTracks";
 
 const PopularTracks = () => {
 	const params = useParams();
+	// rerender to check "green" or "transparent"
+	const [rerender, setRerender] = useState<boolean>();
 
+	const [userLikedTrackArray, setUserLikedTrackArray] = useState<string[]>([]);
+
+	const [accessToken, setAccessToken] = useState<string>(
+		localStorage.getItem("accessToken") || ""
+	);
 	const artistTopTracks = getArtistTopTracks(params.id || "");
+	const handleTrackLike = (trackId: string) => {
+		console.log("click popular track");
+		const url = `https://api.spotify.com/v1/me/tracks`;
+
+		const body = {
+			ids: [trackId],
+		};
+		putData(url, "PUT", body, accessToken, setAccessToken, () => {
+			//* refresh the copmonent
+			setRerender((prev) => {
+				console.log("rerender like");
+				return !prev;
+			});
+		});
+		window.dispatchEvent(new Event("libraryModified"));
+	};
+	const handleTrackUnLike = (trackId: string) => {
+		const url = `https://api.spotify.com/v1/me/tracks`;
+
+		const body = {
+			ids: [trackId],
+		};
+		putData(url, "DELETE", body, accessToken, setAccessToken, () => {
+			//* refresh the component
+			setRerender((prev) => {
+				console.log("rerender delete");
+				return !prev;
+			});
+		});
+		window.dispatchEvent(new Event("libraryModified"));
+	};
+	useEffect(() => {
+		const tracksIdArray: string[] = [];
+		artistTopTracks.data?.tracks.forEach((track) => {
+			tracksIdArray.push(track.id);
+			// console.log("items", track);
+		});
+
+		const tracksIdQueries = tracksIdArray.join(",");
+		const url = `https://api.spotify.com/v1/me/tracks/contains?ids=${tracksIdQueries}`;
+		checkUserHasTracks(url, "GET", accessToken, setAccessToken, (data) => {
+			setUserLikedTrackArray(data);
+		});
+		console.log("userLikedTrackArray", userLikedTrackArray);
+	}, [rerender]);
 	// console.log(artistTopTracks);
+	useEffect(() => {
+		if (localStorage.getItem("accessToken")) {
+			// console.log("YES-accesstoken-fetchdata");
+			setAccessToken(localStorage.getItem("accessToken") || "");
+		} else {
+			console.log("NO-accesstoken-fetchdata");
+		}
+	}, []);
+	if (artistTopTracks.isLoading) return <p>Loading...</p>;
 	return (
 		<Container>
 			<h2>Popular</h2>
-			{artistTopTracks.data?.tracks.map((track, index) => (
-				<TrackItem key={track.id}>
-					<div>
-						<span>{index + 1}</span>
-						<button>
-							<img src="/icons/playTrack.svg" />
-						</button>
-					</div>
-					<div>
-						<img src={track.album.images[0].url} />
+			{artistTopTracks &&
+				artistTopTracks.data?.tracks.map((track, index) => (
+					<TrackItem key={track.id}>
 						<div>
-							<Link to="">
-								<span>{track.name}</span>
-							</Link>
+							<span>{index + 1}</span>
+							<button>
+								<img src="/icons/playTrack.svg" />
+							</button>
 						</div>
-					</div>
+						<div>
+							<img src={track.album.images[0].url} />
+							<div>
+								<Link to="">
+									<span>{track.name}</span>
+								</Link>
+							</div>
+						</div>
 
-					<div>
-						<button>
-							<img src="/icons/heart.svg" />
-						</button>
-						<span>{msToMin(track.duration_ms)}</span>
-						<button>
-							<img src="/icons/threedots.svg" />
-						</button>
-					</div>
-				</TrackItem>
-			))}
+						<div>
+							<button>
+								{userLikedTrackArray[index] ? (
+									<img
+										src="/public/icons/heartGreen.svg"
+										onClick={() => handleTrackUnLike(track.id)}
+									/>
+								) : (
+									<img
+										src="/icons/heart.svg"
+										onClick={() => handleTrackLike(track.id)}
+									/>
+								)}
+							</button>
+							<span>{msToMin(track.duration_ms)}</span>
+							<button>
+								<img src="/icons/threedots.svg" />
+							</button>
+						</div>
+					</TrackItem>
+				))}
 		</Container>
 	);
 };
