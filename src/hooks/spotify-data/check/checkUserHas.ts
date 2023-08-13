0331d49e0d40c945db136a8b5142386f78ba67b1
kobbin003 <dkfeto@gmail.com>
2023-08-13 16:49:22 +0530

    checkuserhas fetch

---
+++ src/hooks/spotify-data/check/checkUserHas.ts
@@ -0,0 +1,79 @@
+const checkUserHas = (
+	url: string,
+	method: string,
+	accessToken: string,
+	setAccessToken: React.Dispatch<React.SetStateAction<string>>,
+	callback: (data: boolean[]) => void
+) => {
+	// console.log("CHECK TRACK CONTAINS");
+	const params = new URL(url).searchParams;
+	// console.log(params.get("type"), params.get("ids"));
+	const hasIdsQueries = params.get("ids") && true;
+	// console.log(hasIdsQueries, params.get("ids"));
+	const options = {
+		method,
+		headers: {
+			"Content-Type": "Application/json",
+			Authorization: `Bearer ${accessToken}`,
+		},
+	};
+	if (accessToken && hasIdsQueries) {
+		// console.log("accesstoken-fetchdata");
+		fetch(url, options)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					console.log(response.status);
+				}
+			})
+			.then((data) => {
+				// console.log("checkdata", data);
+				callback(data);
+				return data;
+			})
+			.then((res) => {
+				if (res === ("The access token expired" || "Invalid access token")) {
+					console.log("putData: access token expired");
+					//! USE refresh token to get a new access_token
+					const client_id = import.meta.env.VITE_CLIENT_ID;
+					const client_secret = import.meta.env.VITE_CLIENT_SECRET;
+					const payload = client_id + ":" + client_secret;
+					const encodedPayload = window.btoa(payload);
+					let queryParams = new URLSearchParams();
+					const refreshToken = localStorage.getItem("refreshToken");
+					queryParams.set("grant_type", "refresh_token");
+					queryParams.set("refresh_token", refreshToken || "");
+					const url = `https://accounts.spotify.com/api/token?${queryParams}`;
+					const options = {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded",
+							Authorization: `Basic ${encodedPayload}`,
+						},
+						json: true,
+					};
+
+					return fetch(url, options);
+				}
+			})
+			.then((res) => res?.json())
+			.then((res) => {
+				if (res) {
+					localStorage.setItem("accessToken", res?.access_token);
+					// set access token so that fetch is re-performed with the "renewed" token
+					setAccessToken(res?.access_token);
+				} else {
+					console.log("token not expired");
+				}
+			})
+			.catch((err) => {
+				// setIsLoading(false);
+				// setError(err);
+				// errorfetched = error;
+				console.log(err);
+			});
+	}
+};
+
+export default checkUserHas;
