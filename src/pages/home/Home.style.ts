c25a1ae879e9a8cbb807988fcd53a6544c857cfd
kobbin003 <dkfeto@gmail.com>
2023-08-23 12:52:52 +0530

    Home - show new releases

--- src/pages/home/Home.style.ts
+++ src/pages/home/Home.style.ts
@@ -2,75 +2,154 @@ import styled from "styled-components";
 import { style } from "styled-system";
 
 export const Container = styled.div`
-	/* height: 500px; */
 	height: 100vh;
 	width: 100%;
 	background-color: var(--black-dark);
-	/* background-color: goldenrod; */
-	/* padding-top: 90px; */
-	/* min-width: 550px; */
-	/* overflow: hidden; */
 `;
-export const SubHeaders = styled.div`
-	display: flex;
-	justify-content: space-between;
-	padding: 1em;
-	h2 {
-		min-width: 200px;
-	}
-	a {
-		text-decoration: none;
-		color: var(--font-grey);
-		font-size: 0.8rem;
-		align-self: center;
-		min-width: 50px;
-	}
-`;
-export const Albums = styled.div`
-	display: flex;
-	/* width: 100%; */
-	justify-content: space-between;
-	/* flex-wrap: wrap; */
-	padding: 1em;
-	overflow: hidden;
-	min-width: 300px;
-	@media only screen and (max-width: 600px) {
+export const CardsContainer = styled.div`
+	display: grid;
+	width: 100%;
+	position: relative;
+	cursor: pointer;
+	padding-bottom: 3em;
+	/* grid: repeat(2, 1fr) / repeat(4, minmax(150px, 300px)); */
+	/* min-width: 200px; */
+	@media only screen and (max-width: 500px) {
 		/* background-color: pink; */
-		a:nth-child(n + 3) {
-			display: none;
-		}
+		/* grid: auto / minmax(250px, auto); */
+		grid-template-columns: repeat(1, minmax(200px, auto));
+		grid-auto-rows: minmax(100px, 250px);
+		/* border: 2px solid green; */
 	}
 
 	/* Small devices (portrait tablets and large phones, 600px and up) */
-	@media (min-width: 600px) and (max-width: 768px) {
+	@media (min-width: 500px) and (max-width: 768px) {
 		/* background-color: yellow; */
-		a:nth-child(n + 4) {
-			display: none;
-		}
+		grid: auto/repeat(2, minmax(150px, auto));
 	}
 
 	/* Medium devices (landscape tablets, 768px and up) */
-	@media (min-width: 768px) and (max-width: 992px) {
+	@media (min-width: 768px) and (max-width: 892px) {
 		/* background-color: blueviolet; */
-		a:nth-child(n + 5) {
-			display: none;
-		}
+		grid: auto/repeat(3, minmax(150px, auto));
 	}
 
 	/* Large devices (laptops/desktops, 992px and up) */
-	@media (min-width: 992px) and (max-width: 1200px) {
+	@media (min-width: 892px) and (max-width: 1200px) {
 		/* background-color: wheat; */
-		a:nth-child(n + 6) {
-			display: none;
-		}
+		/* grid: auto/auto auto auto auto; */
+		grid: auto/repeat(4, minmax(150px, 250px));
 	}
 
 	/* Extra large devices (large laptops and desktops, 1200px and up) */
-	@media (min-width: 1200px) {
+	@media (min-width: 1200px) and (max-width: 1400px) {
 		/* background-color: crimson; */
-		a:nth-child(n + 7) {
-			display: none;
+		/* grid: auto/auto auto auto auto auto; */
+		grid: auto/repeat(5, minmax(150px, auto));
+	}
+	/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media (min-width: 1400px) {
+		/* background-color: blue; */
+		/* grid: auto/ auto auto auto auto auto auto; */
+		grid: auto/repeat(6, minmax(150px, auto));
+	}
+`;
+export const ResultCard = styled.div`
+	position: relative;
+	background-color: var(--data-display-background);
+	padding: 1em;
+	border-radius: 5px;
+	/* padding-top: 1em; */
+	margin-right: 0.8em;
+	margin-bottom: 0.8em;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	transition: background-color 0.2s ease-in-out;
+	& > a {
+		text-decoration: none;
+		color: white;
+	}
+	p:nth-of-type(1) {
+		display: block;
+		padding-top: 0.6em;
+		padding-bottom: 0.6em;
+		margin-bottom: 0.4em;
+		font-size: 1.5rem;
+		/* max-width: 4em; */
+		white-space: nowrap;
+		overflow-x: scroll;
+		/* border: 1px solid greenyellow; */
+		overflow-y: hidden;
+		::-webkit-scrollbar {
+			width: 1px;
+			height: 1px;
 		}
+
+		::-webkit-scrollbar-track {
+			background-color: #acabab;
+		}
+
+		::-webkit-scrollbar-thumb {
+			background-color: green;
+		}
+	}
+	p:nth-of-type(2) {
+		background-color: var(--black-darkest);
+		width: fit-content;
+		font-weight: 600;
+		padding: 0.4em;
+		border-radius: 6666px;
+	}
+	@media only screen and (max-width: 500px) {
+		/* height: 70%; */
+		p:nth-of-type(1) {
+			font-size: 1rem;
+			padding-top: 0.3em;
+			padding-bottom: 0.3em;
+			margin-bottom: 0em;
+			/* height: 20px; */
+			/* padding-top: 0; */
+		}
+	}
+	&:hover {
+		background-color: var(--data-display-background-hover);
+		img:nth-of-type(2) {
+			visibility: visible;
+			/* padding-bottom: 1em; */
+			translate: 0 -10px;
+		}
+	}
+`;
+
+export const ImageContainer = styled.div`
+	position: relative;
+	/* height: 80%; */
+	contain: content;
+	img:nth-of-type(1) {
+		width: 100%;
+		border-radius: 50%;
+	}
+	img:nth-of-type(2) {
+		position: fixed;
+		width: 30%;
+		border-radius: 50%;
+		right: 0;
+		bottom: -10px;
+		/* margin: 1em; */
+		/* margin-bottom: 0; */
+		visibility: hidden;
+		transition: translate 0.1s ease-in, visibility 0.1s ease-in;
+	}
+	@media only screen and (max-width: 500px) {
+		img:nth-of-type(1) {
+			height: 100%;
+			width: 100%;
+		}
+		img:nth-of-type(2) {
+			width: 25%;
+		}
+		height: 70%;
 	}
 `;
 //! adjust album wrt to media
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,69 +3,88 @@ import AlbumCards from "../../components/cards/AlbumCards/AlbumCards";
 import getUserAlbums, {
 	UserAlbums,
 } from "../../hooks/spotify-data/getUserAlbums";
-import { Albums, Container, SubHeaders } from "./Home.style";
-import getCurrentUserProfile from "../../hooks/spotify-data/getUser";
-import getNewReleases from "../../hooks/spotify-data/getNewReleases";
-import getCurrentUserPlaylist from "../../hooks/spotify-data/getUserPlaylist";
+import {
+	CardsContainer,
+	Container,
+	ImageContainer,
+	ResultCard,
+} from "./Home.style";
+import getNewReleases, {
+	Albums,
+} from "../../hooks/spotify-data/getNewReleases";
+import { Link, useOutletContext } from "react-router-dom";
+import getArtist from "../../hooks/spotify-data/getArtist";
 
 const Home = () => {
-	// const [accessToken, setAccessToken] = useState(
-	// 	localStorage.getItem("accessToken")
-	// );
-	const accessToken = localStorage.getItem("accessToken") || "";
-	// const { data, error, isLoading } = getUserAlbums(accessToken);
-	// const { data, error, isLoading } = getCurrentUserProfile(accessToken);
-	// const { data, error, isLoading } = getNewReleases(accessToken);
-	// const { data, error, isLoading } = getCurrentUserPlaylist(accessToken);
-	// console.log("HOME", accessToken, data, error, isLoading);
-
-	// localStorage.setItem("albumdata", JSON.stringify(data));
-
-	// const albumdata = localStorage.getItem("albumdata");
-	// const data: UserAlbums = JSON.parse(albumdata ?? "");
-	// console.log(data);
-
-	//* Home layout:
-	// New ALbums
-	// Based on saved albums and tracks
-
+	// console.log("HOME", localStorage.getItem("accessToken"));
+	const accessToken = localStorage.getItem("accessToken");
+	const [offset, setOffset] = useState<number>(0);
+	const [releaseData, setReleaseData] = useState<Albums["items"]>([]);
+	const { data, error, isLoading } = getNewReleases([offset, accessToken]);
+	// console.log(data, releaseData);
+	useEffect(() => {
+		if (data) {
+			setReleaseData((prev) => [...prev, ...data?.albums.items]);
+		}
+	}, [data]);
+	if (isLoading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error.message}</p>;
 	return (
 		<Container>
-			<h1>Home</h1>
-			<>
-				{/* <SubHeaders>
-					<h2>Saved Albums</h2>
-					<a href="">
-						<b>show all</b>
-					</a>
-				</SubHeaders>
-				<Albums>
-					{data &&
-						data.items.map((item) => (
-							<AlbumCards
-								key={item.album.id}
-								src={item.album.images[1].url}
-								height={item.album.images[1].height}
-								width={item.album.images[1].width}
-								albumName={item.album.name}
-								releaseDate={item.album.release_date}
-								type={item.album.album_type}
-							></AlbumCards>
-						))}
-					{data &&
-						data.items.map((item) => (
-							<AlbumCards
-								key={item.album.id}
-								src={item.album.images[1].url}
-								height={item.album.images[1].height}
-								width={item.album.images[1].width}
-								albumName={item.album.name}
-								releaseDate={item.album.release_date}
-								type={item.album.album_type}
-							></AlbumCards>
-						))}
-				</Albums> */}
-			</>
+			<h1
+				style={{
+					paddingBottom: "0.5em",
+					paddingTop: "0.5em",
+					whiteSpace: "nowrap",
+				}}
+			>
+				Recent Releases
+			</h1>
+			<CardsContainer>
+				{releaseData
+					? releaseData.map((item) => (
+							<ResultCard key={item.id}>
+								<Link to={`/me/album/${item.id}`}>
+									<ImageContainer>
+										{item.images.length > 0 ? (
+											<img src={item.images[0].url}></img>
+										) : (
+											<img src="/public/icons/defaultCover.svg"></img>
+										)}
+										<img src="/icons/spotify_play.svg"></img>
+									</ImageContainer>
+									<p>
+										<b>{item.name}</b>
+									</p>
+									<p>{item.type[0].toUpperCase() + item.type.slice(1)}</p>
+								</Link>
+							</ResultCard>
+					  ))
+					: data?.albums.items.map((item) => (
+							<ResultCard key={item.id}>
+								<Link to={`/me/album/${item.id}`}>
+									<ImageContainer>
+										{item.images.length > 0 ? (
+											<img src={item.images[0].url}></img>
+										) : (
+											<img src="/public/icons/defaultCover.svg"></img>
+										)}
+										<img src="/icons/spotify_play.svg"></img>
+									</ImageContainer>
+									<p>
+										<b>{item.name}</b>
+									</p>
+									<p>{item.type[0].toUpperCase() + item.type.slice(1)}</p>
+								</Link>
+							</ResultCard>
+					  ))}
+			</CardsContainer>
+			<button
+				onClick={() => setOffset((prev) => (prev += 25))}
+				style={{ marginBottom: "2em" }}
+			>
+				show more
+			</button>
 		</Container>
 	);
 };
