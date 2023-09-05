cc6f5f2e35735c03b2b194108ac981b81b4718ea
kobbin003 <dkfeto@gmail.com>
2023-09-05 13:37:28 +0530

    error as data handled

--- src/hooks/spotify-data/checkUserHasTracks/checkUserHasTracks.ts
+++ src/hooks/spotify-data/checkUserHasTracks/checkUserHasTracks.ts
@@ -3,9 +3,15 @@ const checkUserHasTracks = (
 	method: string,
 	accessToken: string,
 	setAccessToken: React.Dispatch<React.SetStateAction<string>>,
-	callback: (data: string[]) => void
+	callback: (
+		data: boolean[] | null,
+		error: { status: number; message: string } | null
+	) => void
 ) => {
 	// console.log("CHECK TRACK CONTAINS");
+	const params = new URL(url).searchParams;
+	const hasIdsQueries = params.get("ids") && true;
+	// console.log(params.get("type"), params.get("ids"));
 	const options = {
 		method,
 		headers: {
@@ -13,24 +19,27 @@ const checkUserHasTracks = (
 			Authorization: `Bearer ${accessToken}`,
 		},
 	};
-	if (accessToken) {
+	if (accessToken && hasIdsQueries) {
+		// console.log(url.split("?")[1].split("=")[1]);
 		// console.log("accesstoken-fetchdata");
 		fetch(url, options)
 			.then((response) => {
-				if (response.ok) {
-					return response.json();
-				} else {
-					console.log(response.status);
-				}
+				// if (response.ok) {
+				return response.json();
+				// }
 			})
 			.then((data) => {
-				// console.log("checkdata", data);
-				callback(data);
+				// console.log("checkdata", data.error);
+				if (data.error) {
+					callback(null, data.error);
+				} else {
+					callback(data, null);
+				}
 				return data;
 			})
 			.then((res) => {
 				if (res === ("The access token expired" || "Invalid access token")) {
-					console.log("putData: access token expired");
+					// console.log("putData: access token expired");
 					//! USE refresh token to get a new access_token
 					const client_id = import.meta.env.VITE_CLIENT_ID;
 					const client_secret = import.meta.env.VITE_CLIENT_SECRET;
@@ -60,14 +69,14 @@ const checkUserHasTracks = (
 					// set access token so that fetch is re-performed with the "renewed" token
 					setAccessToken(res?.access_token);
 				} else {
-					console.log("token not expired");
+					// console.log("token not expired");
 				}
 			})
 			.catch((err) => {
 				// setIsLoading(false);
 				// setError(err);
 				// errorfetched = error;
-				console.log(err);
+				// callback("hi");
 			});
 	}
 };
