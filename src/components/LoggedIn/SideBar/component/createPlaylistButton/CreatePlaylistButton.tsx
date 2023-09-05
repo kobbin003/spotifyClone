97cdc21fb70a814598b416813458ff6af521971b
kobbin003 <dkfeto@gmail.com>
2023-09-05 13:36:00 +0530

    CreatePlaylistButton component

--- src/components/LoggedIn/SideBar/component/createPlaylistButton/CreatePlaylistButton.tsx
+++ src/components/LoggedIn/SideBar/component/createPlaylistButton/CreatePlaylistButton.tsx
@@ -1,21 +1,35 @@
-import React, { MouseEvent, useContext, useState } from "react";
+import React, { MouseEvent, useContext, useEffect, useState } from "react";
 import { HoverMessageItem } from "../../style";
 import { ShowModalContext } from "../../../../../layout/LoggedInLayout";
+import createPlaylist from "../../../../../hooks/spotify-data/playlist/createPlaylist";
 
 /** requiired: user_id */
-const CreatePlaylistButton = () => {
-	// const url = `https://api.spotify.com/v1/users/{user_id}/playlists`;
-	// const method = "PUT";
-	// const body = {
-	// 	name: "New Playlist",
-	// 	description: "New playlist description",
-	// 	public: false,
-	// };
-	const setShowModal = useContext(ShowModalContext);
+const CreatePlaylistButton = ({ id }: { id: string }) => {
+	const [accessToken, setAccessToken] = useState<string>("");
+
 	const handleCreatePlaylist = () => {
 		// console.log("modal context", setShowModal);
-		setShowModal((prev) => !prev);
+		// setShowModal((prev) => !prev);
+		const url = `https://api.spotify.com/v1/users/${id}/playlists`;
+		createPlaylist(
+			url,
+			"POST",
+			{ name: "playlist", description: "", public: true },
+			accessToken,
+			setAccessToken,
+			() => {
+				window.dispatchEvent(new Event("playlistLibraryModified"));
+			}
+		);
 	};
+
+	useEffect(() => {
+		const token = localStorage.getItem("accessToken");
+		if (token) {
+			setAccessToken(token);
+		}
+	}, []);
+
 	return (
 		<>
 			<button onClick={handleCreatePlaylist}>
