0c641223b6a1ad4cc4588cdabf0c726cbb38a115
kobbin003 <dkfeto@gmail.com>
2023-09-12 12:52:49 +0530

    userProfile context s value set

--- src/components/LoggedIn/MainContent/MainContent.tsx
+++ src/components/LoggedIn/MainContent/MainContent.tsx
@@ -1,21 +1,47 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import NavBar from "../NavBar/NavBar";
 import { Container, Content } from "./style";
-import SearchTypesList from "../../searchTypesList/SearchTypesList";
+import { UserProfileContext } from "../../../layout/LoggedInLayout";
+import getUserProfile from "../../../hooks/spotify-data/getUserProfile";
 
 type Prop = {
 	left: number;
 	widthHandleDragger: number;
 };
+export type ContexttType = [
+	string,
+	number,
+	number,
+	React.Dispatch<React.SetStateAction<"hidden" | "scroll">>,
+	number,
+	React.Dispatch<React.SetStateAction<number>>
+];
 
 const MainContent = ({ left, widthHandleDragger }: Prop) => {
-	const [queryFromSearchBar, setQueryFromSearchBar] = useState<string>();
+	const [queryFromSearchBar, setQueryFromSearchBar] = useState<
+		string | undefined
+	>("");
+
 	const [showSearchTypes, setShowSearchTypes] = useState<boolean>(false);
+
 	const [scroll, setScroll] = useState<"scroll" | "hidden">("scroll");
+	const [scrolledY, setScrolledY] = useState(0);
+
+	const userProfileContext = useContext(UserProfileContext);
+	const { data, error, isLoading } = getUserProfile();
+
 	const location = window.location.pathname;
-	const isInSearchRoute = /^\/me\/search.*/.test(location);
+
+	// since we cannot fetch the data in <LoggedInLayout />, we are setting the userProfile here.
+	useEffect(() => {
+		if (data) {
+			userProfileContext?.setUserProfile(data);
+		}
+		console.log(userProfileContext?.userProfile);
+	}, [data]);
+
 	useEffect(() => {
 		// console.log("location change", location);
 		if (location === "/me/") {
@@ -23,7 +49,32 @@ const MainContent = ({ left, widthHandleDragger }: Prop) => {
 		}
 		//soething new
 	}, [location]);
-	// console.log("maincontent", queryFromSearchBar);
+
+	// console.log("maincontent", localStorage.getItem("accessToken"));
+	useEffect(() => {
+		const el = document.getElementById("container") as HTMLElement;
+		const handleScroll = (e: any) => {
+			// console.log(
+			// 	"scroll event:",
+			// 	el.scrollTop,
+			// 	el.scrollHeight,
+			// 	el.clientHeight
+			// );
+			setScrolledY(el.clientHeight + el.scrollTop);
+		};
+		el.addEventListener("scroll", handleScroll);
+		return () => el.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	useEffect(() => {
+		const el = document.getElementById("container") as HTMLElement;
+		// TODO !!! this is not working
+		el.scrollTop = 100;
+	}, []);
+
+	useEffect(() => {
+		// console.log("scrolledY", scrolledY);
+	}, [scrolledY]);
 
 	return (
 		<>
@@ -31,6 +82,7 @@ const MainContent = ({ left, widthHandleDragger }: Prop) => {
 				left={left}
 				widthHandleDragger={widthHandleDragger}
 				scroll={scroll}
+				id="container"
 			>
 				<NavBar
 					left={left}
@@ -44,7 +96,14 @@ const MainContent = ({ left, widthHandleDragger }: Prop) => {
 		)} */}
 				<Content>
 					<Outlet
-						context={[queryFromSearchBar, left, widthHandleDragger, setScroll]}
+						context={[
+							queryFromSearchBar,
+							left,
+							widthHandleDragger,
+							setScroll,
+							scrolledY,
+							setScrolledY,
+						]}
 					/>
 					{/* <Outlet /> */}
 				</Content>
